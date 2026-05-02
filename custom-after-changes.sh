50d781905bdd8f62acd930b07a68044640598fad
nathanael <me@nattho.com>
2026-05-02 16:50:05 +0100

    rename

---
+++ custom-after-changes.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Put custom commands here that you would like to run at the end of the script
+# eg. since symfony does not support auto app down before deployment in classical mode, you may put your own logic to put the app back up here, if you had put it down
+# Code here will be executed from the current directory(if symlink method is used) or the current app directory(if classical method is used), so be sure to cd to the correct directory if needed
+
--- custom-before-linking.sh
+++ custom-before-linking.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Put custom commands here that you would like to run just before linking the new release directory to current
+# Put custom commands here that you would like to run just before linking the new release directory to current(symlink method only)
 # eg. A custom asset building command not included in teh script
-# Code here will be executed from the release directory(if symlink method is used) or the current app directory(if classical method is used), so be sure to cd to the correct directory if needed
+# Code here will be executed from the release directory
 
