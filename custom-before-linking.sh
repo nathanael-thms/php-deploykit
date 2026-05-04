7c8673f1bb249dbee09875cdacb205016a7f4ee4
nathanael <me@nattho.com>
2026-05-04 10:03:09 +0100

    fix spelling error

--- custom-before-linking.sh
+++ custom-before-linking.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # Put custom commands here that you would like to run just before linking the new release directory to current(symlink method only)
-# eg. A custom asset building command not included in teh script
+# eg. A custom asset building command not included in the script
 # Code here will be executed from the release directory
 
